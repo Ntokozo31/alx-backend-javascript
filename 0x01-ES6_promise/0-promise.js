236265314f70f91aec0c6d102a8c7af3272c16a4
Ntokozo31 <dubentokozo45@yahoo.com>
2024-10-05 11:36:01 +0200

    0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,18 +1,18 @@
 function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-        const success = true;
-        if (success) {
-            resolve('success');
-        } else {
-            reject('404!');
-        }
-    });
+  return new Promise((resolve, reject) => {
+    const success = true;
+    if (success) {
+      resolve('success');
+    } else {
+      reject(new Error('404!'));
+    }
+  });
 }
 
 getResponseFromAPI()
-    .then((response) => {
-         console.log(response);
-    })
-    .catch((error) => {
-         console.error(error);
-    });    
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
