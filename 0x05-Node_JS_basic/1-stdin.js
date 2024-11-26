4fc0f3741c5add0af3e6ff61df1ab6fa25113df8
Ntokozo31 <dubentokozo45@yahoo.com>
2024-11-26 21:59:24 +0200

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,9 @@
 process.stdin.setEncoding('utf-8');
 console.log('Welcome to Holberton School, what is your name?\n');
-process.stdin.on('data', function(data) {
-    console.log(`Your name is: ${data}`);
+process.stdin.on('data', (data) => {
+  console.log(`Your name is: ${data}`);
 });
-process.stdin.on('end', function() {
-    console.log('This important software is now closing\n');
-    process.exit();
+process.stdin.on('end', () => {
+  console.log('This important software is now closing\n');
+  process.exit();
 });
